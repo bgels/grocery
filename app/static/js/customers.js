c57ee8bfccd4baeb0fcdca67e07f54c6bbc06a28
Yu Lu <ylu60@stuy.edu>
2026-05-05 11:00:04 -0400

    json templates in js

--- app/static/js/customers.js
+++ app/static/js/customers.js
@@ -1,19 +1,23 @@
 const CUSTOMERS = [
     {
-        id: "regular_1",
-        name: "Regular Customer",
-        sprite: "static/images/npc/",
-        isSpecial : false
+      id: "regular_1",
+      name: "Regular Customer",
+      sprite: "static/images/npc/",
+      traits: ""
     }
 ];
-const SPECIAL_CUSTOMERS = [];
+const SPECIAL_CUSTOMERS = [
+  {
+    id: "regular_1",
+    name: "Regular Customer",
+    sprite: "static/images/npc/",
+  }
+];
 const NAME_PARTS = {
     first:[],
     last:[]
 };
 const CLOTHING = {};
-
-
 const DIALOGUE = [];
 const TRAITS = [];
 // Calcualte dialoge based on traits that the npc has? special npcs will use special dialogue
