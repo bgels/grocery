2ac05ac370823be9123de72f668a03a53290776b
Emily Mai <emilym384@nycstudents.net>
2026-05-12 11:00:14 -0400

    database tables

--- app/build_db.py
+++ app/build_db.py
@@ -1,13 +1,65 @@
-# Orangutans
-# Kiran Soemardjo, Eviss Wu, Mustafa Abdullah, Yu Lu
+# Restaurant Simulator
+# Yu Lu, Ricky Lin, Jun Jie Li, Emily Mai
 # SoftDev
 
-#from api import
-from db import general_query, insert_query, select_query
-import json
-import numpy
+import sqlite3
 
-def create_tables():
-    return None
+DB_FILE = "data.db"
 
-create_tables()
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+
+c.executescript("""
+    DROP TABLE IF EXISTS User;
+    CREATE TABLE User (
+        user_id TEXT PRIMARY KEY,
+        password TEXT
+    );
+
+    DROP TABLE IF EXISTS Game;
+    CREATE TABLE Game (
+        day TEXT,
+        hour INTEGER,
+        money INTEGER,
+        served INTEGER,
+        killed INTEGER,
+        revenue INTEGER,
+        FOREIGN KEY (user_id) REFERENCES User(user_id),
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    );
+
+    DROP TABLE IF EXISTS Items;
+    CREATE TABLE Items (
+        name TEXT,
+        amount INTEGER
+    );
+
+    DROP TABLE IF EXISTS Customers;
+    CREATE TABLE Customers (
+        customer_id TEXT PRIMARY KEY,
+        name TEXT,
+        sprite TEXT,
+        traits TEXT,
+        budget INTEGER
+    );
+
+    DROP TABLE IF EXISTS Products;
+    CREATE TABLE Products (
+        name TEXT PRIMARY KEY,
+        quantity INTEGER,
+        buy_price INTEGER,
+        sell_price INTEGER,
+        rarity TEXT
+    );
+
+    DROP TABLE IF EXISTS Upgrades;
+    CREATE TABLE Upgrades (
+        shelf INTEGER,
+        register INTEGER,
+        decor INTEGER,
+        firepower INTEGER
+    );
+""")
+
+db.commit()
+db.close()
