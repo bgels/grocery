9b99b15621e574f137e7c7ebdad5091ef805f4dc
yu <ylu60@stuy.edu>
2026-05-29 08:44:24 -0400

    fixed styling

--- app/__init__.py
+++ app/__init__.py
@@ -145,6 +145,6 @@ def reset():
     return jsonify(success=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
 
 # query the sqlite for yu and so he can use stuff from the database
